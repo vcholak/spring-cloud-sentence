620d0d13eab2e45a5fb22c5c0b20956e6797baae
Volodymyr Cholak <volchok60@yahoo.com>
2017-11-27 15:33:16 -0500

    Added Adjective client.

---
+++ adjective/src/main/java/com/rutheniaitc/adjective/AdjectiveApplication.java
@@ -0,0 +1,14 @@
+package com.rutheniaitc.adjective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AdjectiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdjectiveApplication.class, args);
+	}
+}
