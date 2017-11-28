3c47eaf1319c5e35b9c11747ccf1427144dd60bb
Volodymyr Cholak <volchok60@yahoo.com>
2017-11-28 12:23:05 -0500

    Added comment.

--- subject/src/test/java/com/rutheniaitc/subject/SubjectApplicationTests.java
+++ subject/src/test/java/com/rutheniaitc/subject/SubjectApplicationTests.java
@@ -5,6 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * Integration test
+ * Spring Cloud Config server must be running
+ * @author vcholak
+ *
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SubjectApplicationTests {
